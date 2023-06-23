847edb51ea73911b01644f95ffbdbb6b57744999
Arthur de Oliveira Carvalho <arthur.o.carvalho@gmail.com>
2023-06-23 17:03:09 -0300

    API get user details

--- cypress/e2e/integrations/backend.cy.js
+++ cypress/e2e/integrations/backend.cy.js
@@ -37,7 +37,45 @@ describe("Should test API", () => {
       .then((res) => console.log(res));
   });
 
-  it('shoud delete the user created before', () => {
+  it("should get user details by random ID", () => {
+    cy.request({
+      method: "GET",
+      url: "https://gorest.co.in/public/v2/users",
+      headers: {
+        Authorization:
+          "Bearer 6916c4726abd10bcc789b6c10f5e372da8b2e60b846ca1b192cf83cd74da2616",
+      },
+    }).then((res) => {
+      const userList = res.body.data || [];
+      console.log(userList);
+  
+      if (userList.length > 0) {
+        // Selecionar aleatoriamente um usuário da lista
+        const randomUser = Cypress._.sample(userList);
+        const randomUserId = randomUser.id;
+        console.log(`ID do usuário aleatório: ${randomUserId}`);
+  
+        cy.request({
+          method: "GET",
+          url: `https://gorest.co.in/public/v2/users/${randomUserId}`,
+          headers: {
+            Authorization:
+              "Bearer 6916c4726abd10bcc789b6c10f5e372da8b2e60b846ca1b192cf83cd74da2616",
+          },
+        }).then((userRes) => {
+          expect(userRes.status).to.eq(200);
+          expect(userRes.body.data.id).to.eq(randomUserId);
+          cy.log(userRes.body.data); 
+          console.log(userRes.body.data);
+        });
+      } else {
+        console.log("A lista de usuários está vazia.");
+      }
+    });
+  });
+  
+
+  it("shoud delete the user created before", () => {
     cy.request({
       method: "DELETE",
       url: `https://gorest.co.in/public/v2/users/${createdUserId}`,
@@ -46,13 +84,12 @@ describe("Should test API", () => {
           "Bearer 6916c4726abd10bcc789b6c10f5e372da8b2e60b846ca1b192cf83cd74da2616",
       },
       body: {
-        id: createdUserId
-      }
+        id: createdUserId,
+      },
     }).then((res) => {
-      console.log(res)
-      expect(res.status).to.eq(204)
-      cy.log(`StatusText: ${res.statusText}`)
-    })
-  })
-  
+      console.log(res);
+      expect(res.status).to.eq(204);
+      cy.log(`StatusText: ${res.statusText}`);
+    });
+  });
 });
