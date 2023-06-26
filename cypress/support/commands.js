fb01587a28fbe31db424f639761de38ef8d948b1
Arthur de Oliveira Carvalho <arthur.o.carvalho@gmail.com>
2023-06-25 21:59:15 -0300

    search bar test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,46 +25,54 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('acessNaveeAutomation', () => {
+Cypress.Commands.add("acessNaveeAutomation", () => {
+  cy.visit("/");
+  // Cypress.on(
+  //     "uncaught:exception", (err, runnable) => true
+  // );
+}); //Função realiza acesso a plataforma(tratando erros)
 
-    cy.visit('/');
-    // Cypress.on(
-    //     "uncaught:exception", (err, runnable) => true
-    // );
-});//Função realiza acesso a plataforma(tratando erros)
+Cypress.Commands.add("setLoginData", (email, password) => {
+  cy.get(".caret").click();
+  cy.contains("Login").click();
+  cy.url().should("include", "index.php?route=account/login");
+  cy.contains("Returning Customer").should("be.visible");
+  cy.get("#input-email").type(email);
+  cy.get("#input-email").should("have.value", email);
+  cy.get("#input-password").type(password);
+  cy.get("#input-password").should("have.value", password);
+  cy.get("[type=submit][value=Login]").click();
+});
 
-Cypress.Commands.add('setLoginData', (email, password) =>{
-    cy.get('.caret').click()
-    cy.contains('Login').click()
-    cy.url().should('include', 'index.php?route=account/login');
-    cy.contains('Returning Customer').should('be.visible')
-    cy.get('#input-email').type(email)
-    cy.get('#input-email').should('have.value', email)
-    cy.get('#input-password').type(password)
-    cy.get('#input-password').should('have.value', password)
-    cy.get('[type=submit][value=Login]').click()
-})
-
-Cypress.Commands.add('setNewUserFields', (firstName, lastName, email, phone, password, confPassword) =>{
+Cypress.Commands.add(
+  "setNewUserFields",
+  (firstName, lastName, email, phone, password, confPassword) => {
     cy.get(".caret").click();
     cy.contains("Register").click();
     cy.contains("Register Account").should("be.visible");
-    cy.get('#input-firstname').type(firstName)
-    cy.get('#input-lastname').type(lastName)
-    cy.get('#input-email').type(email)
-    cy.get('#input-telephone').type(phone)
-    cy.get('#input-password').type(password)
-    cy.get('#input-confirm').type(confPassword)
-    cy.get("label input[type='radio']").check('1', { force: true });
-    cy.get('[type="checkbox"], [name="agree"]').check('1')
-    cy.log('------/ Assertions /------')
-    cy.get('#input-firstname').should('have.value', firstName)
-    cy.get('#input-lastname').should('have.value', lastName)
-    cy.get('#input-email').should('have.value', email)
-    cy.get('#input-telephone').should('have.value', phone)
-    cy.get('#input-password').should('have.value', password)
-    cy.get('#input-confirm').should('have.value', confPassword)
-    cy.get("label input[type='radio'][value='1']").should('be.checked')
-    cy.get('[type="checkbox"], [name="agree"]').should('be.checked')
-    cy.contains('Continue').click()
-})
+    cy.get("#input-firstname").type(firstName);
+    cy.get("#input-lastname").type(lastName);
+    cy.get("#input-email").type(email);
+    cy.get("#input-telephone").type(phone);
+    cy.get("#input-password").type(password);
+    cy.get("#input-confirm").type(confPassword);
+    cy.get("label input[type='radio']").check("1", { force: true });
+    cy.get('[type="checkbox"], [name="agree"]').check("1");
+    cy.log("------/ Assertions /------");
+    cy.get("#input-firstname").should("have.value", firstName);
+    cy.get("#input-lastname").should("have.value", lastName);
+    cy.get("#input-email").should("have.value", email);
+    cy.get("#input-telephone").should("have.value", phone);
+    cy.get("#input-password").should("have.value", password);
+    cy.get("#input-confirm").should("have.value", confPassword);
+    cy.get("label input[type='radio'][value='1']").should("be.checked");
+    cy.get('[type="checkbox"], [name="agree"]').should("be.checked");
+    cy.contains("Continue").click();
+  }
+);
+
+Cypress.Commands.add("setProductInSearchBar", (item) => {
+  cy.get('#search').type(`${item}{enter}`);
+  cy.contains(`Search - ${item}`);
+  cy.get('div [class=row]:eq(4)').contains(item, { matchCase: false})
+});
